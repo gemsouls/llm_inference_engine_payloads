001ebcf9be69b63d834d67e6825d81a2676f613f
PanQiWei <594557445@qq.com>
2023-06-02 15:56:01 +0800

    fix local file url problem

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ DEP_DIR = os.path.join(PROJECT_ROOT, "deps")
 version = "0.1.0"
 requirements = [
     "pydantic",
-    f"openai_api_payloads@{os.path.join(DEP_DIR, 'openai_api_payloads-1.0.0-py3-none-any.whl')}"
+    f"openai_api_payloads @ file://localhost/{os.path.join(DEP_DIR, 'openai_api_payloads-1.0.0-py3-none-any.whl')}"
 ]
 include_dirs = ["deps"]
 
