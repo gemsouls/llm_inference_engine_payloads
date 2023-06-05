4f4b4d9d3147934f26354a968ee2f93bee874183
PanQiWei <594557445@qq.com>
2023-06-05 14:11:46 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 import os
 from setuptools import setup, find_packages
 
-
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-DEP_DIR = os.path.join(PROJECT_ROOT, "deps")
-
 version = "0.1.0"
 requirements = [
     "pydantic",
-    f"openai_api_payloads @ file://localhost/{os.path.join(DEP_DIR, 'openai_api_payloads-1.0.0-py3-none-any.whl')}"
+    f"openai_api_payloads @ file://localhost/{os.path.join(os.getcwd(), 'deps', 'openai_api_payloads-1.0.0-py3-none-any.whl')}"
 ]
 include_dirs = ["deps"]
 
